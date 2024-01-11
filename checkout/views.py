7cb235073055563513299d7b403b69403d518918
lukecdev <courtney.luke@outlook.com>
2024-01-11 20:14:06 +0000

    added loading spinner overlay

--- checkout/views.py
+++ checkout/views.py
@@ -50,11 +50,10 @@ def checkout(request):
             'street_address2': request.POST['street_address2'],
             'county': request.POST['county'],
         }
-
         order_form = OrderForm(form_data)
         if order_form.is_valid():
             order = order_form.save(commit=False)
-            pid = request.POST.get('client_secret').split('_secret')[0]
+            pid = request.POST.get("client_secret").split("_secret")[0]
             order.stripe_pid = pid
             order.original_bag = json.dumps(bag)
             order.save()
