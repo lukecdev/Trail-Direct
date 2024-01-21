01d965a89291d51be8e9c80376b208e243dd8e16
lukecdev <courtney.luke@outlook.com>
2024-01-21 17:53:13 +0000

    added review app and model

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import ProductReview
 
-# Register your models here.
+admin.site.register(ProductReview)
+class ProductReviewAdmin(admin.ModelAdmin):
+    list_display = ('user', 'product', 'body', 'approved', 'created_on')
+    list_filter = ('approved', 'created_on')
+    search_fields = ('user', 'product', 'body')
+    actions = ['approve_comments']
+
+    def approve_reviews(self, request, queryset):
+        queryset.update(approved=True)
--- trail_direct/settings.py
+++ trail_direct/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
     'bag',
     'checkout',
     'contact',
+    'reviews',
    
     # Other
     'crispy_forms',
