a7672240cc4f755f2ae3be3411499039a9210091
lukecdev <courtney.luke@outlook.com>
2024-01-23 21:43:59 +0000

    added delete user

--- profiles/urls.py
+++ profiles/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.profile, name='profile'),
     path("profile_update/", views.profile_update, name="profile_update"),
+    path("profile_delete/<int:pk>/", views.profile_delete, name="profile_delete"),
 ]
